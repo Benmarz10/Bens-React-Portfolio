e6fc24c03e21b8b4f32e90507af198f2ae40fda8
Benmarz10 <benjaminmarz10@gmail.com>
2023-04-02 12:32:40 -0500

    Last repo was tangled, redid to fix deployment issues

---
+++ src/pages/Resume.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+function Resume() {
+  return (
+    <Container>
+      <Button
+        href="Benjamin Marz, Resume.pdf"
+        download="Benjamin Marz, Resume.pdf"
+      >
+        Download Resume
+      </Button>
+    
+    </Container>
+  );
+}
+export default Resume;
